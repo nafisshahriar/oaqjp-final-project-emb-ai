d8f668a3c2385406d35f06505215f516ba9b82be
nafis <nafisshahriar003@gmail.com>
2024-10-06 15:35:11 -0400

    Done

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+' ' 'Flask app that uses API to detec emotion in text' ' '
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,9 +6,9 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emo_detect():
+    ' ' 'Detects emotion' ' '
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    
     anger = response['anger']
     disgust = response['disgust']
     fear = response['fear']
@@ -15,11 +16,19 @@ def emo_detect():
     sadness = response['sadness']
     dom_emo = response['dominant_emotion']
 
-    return f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dom_emo}"
+    text=""
+    if anger is None:
+        text += "Invalid text! Please try again!"
+    else:
+        text = f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and "
+        text += f"'sadness': {sadness}. The dominant emotion is {dom_emo}"
+    return text
 
 @app.route("/")
 def render_index_page():
+    ' ' 'Renders HTML ans JS' ' '
     return render_template('index.html')
 
 if __name__ == "__main__":
+
     app.run(host="0.0.0.0", port=5000)
